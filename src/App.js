0ea6e51be5be7b171d13b5093ac25cdeda7a7e5f
choieastsea <choieastsea@gmail.com>
2020-04-09 18:06:47 +0900

    Making React Component by 2 ways
    1. external component (Test in Test.js)
    
    2.
    function component(Subtitle in app.js)
    class component(Subject in app.js)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Test from './Test';
 
+function Subtitle() {
+  return (
+    <h2>written by Choieastsea.</h2>
+  );
+}
+class Subject extends React.Component{
+  render() {
+    return (
+      <h3>Subject: IT</h3>
+    );
+  }
+}
 function App() {
   return (
-    <div>Hello React!</div>
+    <div>
+      <Test />
+      <Subject />
+      <Subtitle />
+      Hello React!
+    </div>
   );
 }
 
