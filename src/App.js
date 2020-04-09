1dbe68229a25baff322ddce9cf455f61ae2bfe71
choieastsea <choieastsea@gmail.com>
2020-04-10 02:35:52 +0900

    setState()
    버튼을 누를때마다 render가 refresh되도록 수정하였다.

--- src/App.js
+++ src/App.js
@@ -13,16 +13,35 @@ class Subject extends React.Component {
     );
   }
 }
-function App() {
-  return (
-    <div>
-      <Test />
-      <Subject subname="IT" />
-      <Subject subname="Math" />
-      <Subtitle name="choieastsea" />
-      Hello React!
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    score: 0
+  };
+  like = () => {
+    this.setState(current => ({ score: current.score + 1 }));
+    //this.state.score += 1;
+    console.log(this.state.score);
+  }
+
+  dislike = () => {
+    this.setState(current => ({ score: this.state.score - 1 }));
+    //this.state.score -= 1;
+    console.log(this.state.score);
+  };
+  render() {
+    return (
+      <div>
+        <Test />
+        <Subject subname="IT" />
+        <Subject subname="Math" />
+        <Subtitle name="choieastsea" />
+        <button onClick={this.like}>Like!</button>
+        <button onClick={this.dislike}>Dislike</button>
+        <br />
+        <span>like? {this.state.score}</span>
+      </div>
+    );
+  };
 }
 
 export default App;
